d35df86061ad61e4cd5efce54501e68657bb3538
raymond301 <raymond.moore301@gmail.com>
2018-10-31 10:08:38 -0500

    outline steps from my catalog build script

--- src/hub/dataload/sources/1000genomes/1000genomes_parser.py
+++ src/hub/dataload/sources/1000genomes/1000genomes_parser.py
@@ -22,6 +22,19 @@ __METADATA__ = {
 # and output a list or a generator of structured JSON objects  #
 ################################################################
 
+# REFDATA_PATH/ALL.wgs.phase3_shapeit2_mvncall_integrated_v5b.20130502.sites.vcf.gz
+# ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.wgs.phase3_shapeit2_mvncall_integrated_v5b.20130502.sites.vcf.gz
+# REFDATA_PATH/ALL.chrMT.phase3_callmom-v0_4.20130502.genotypes.vcf.gz "This will be cat'd with the main file"
+# ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chrMT.phase3_callmom-v0_4.20130502.genotypes.vcf.gz
+# split_keys="AC,AF,AFR_AF,AMR_AF,EAS_AF,EUR_AF,SAS_AF,VT"
+# gunzip -c  $sites_vcf  $mt_vcf_noheader_gz \
+# | perl  $COMMON/VCF_split.pl  -k $split_keys \
+# | bior_vcf_to_tjson  --logfile $TEMP_DIR/bior.log.bior_vcf_to_tjson  2>$TEMP_DIR/stderr.bior_vcf_to_tson \
+# | bior_modify_tjson -f modify_tjson.config  --logfile $TEMP_DIR/bior.log.bior_modify_tjson  2>$TEMP_DIR/stderr.bior_modify_tjson > $TEMP_DIR/intermediate.tjson
+# cat $TEMP_DIR/intermediate.tjson | sort -T $TEMP_DIR/srtTmp -k 1,1 -k 2,2n -k 4,4 -k 5,5 > $TEMP_DIR/intermediate.sort.tjson
+# ruby merge_duplicates.rb $TEMP_DIR/intermediate.sort.tjson > $MAKE_JSON_OUTPUT_FILE_PATH
+
+
 def load_data(infile):
     chrom_list = [str(i) for i in range(1, 23)] + ['X', 'Y', 'MT']
     for chrom in chrom_list:
@@ -32,3 +45,5 @@ def load_data(infile):
         #    _doc['_id'] = doc['_id']
         #    del doc['_id']
         #    yield _doc
+
+
