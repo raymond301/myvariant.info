945b40df0e0be5f11782fcb2285bfaa1dc23d94c
raymond301 <raymond.moore301@gmail.com>
2018-11-13 16:41:15 -0600

    rename all references that contain numeric characters to fix _namespace index issues

--- src/hub/dataload/sources/1000genomes/__init__.py
+++
@@ -1,3 +0,0 @@
-__author__ = 'raymond301'
-from .1000genomes_parser import load_data
-from .1000genomes_dump import KGenomeDumper
---
+++ src/hub/dataload/sources/kgenomes/__init__.py
@@ -0,0 +1,2 @@
+__author__ = 'raymond301'
+from .kgenomes_dump import KGenomeDumper
